b9c17bf13df4e2192667b907cbf2712e46870e9f
binijaaa <sabine.kolesnikova@gmail.com>
2023-04-17 21:27:19 +0300

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqCustomTypeProject
+{
+    internal class Car
+    {
+        private string name;
+        private int unitsSold;
+
+        //constructor to pass values to the object
+        public Car(string name, int unitsSold)
+        {
+            this.name = name;
+            this.unitsSold = unitsSold;
+        }
+
+        public string Name //setters are optional, if fields are public!
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public int UnitsSold
+        {
+            get { return this.unitsSold; }
+            set { this.unitsSold = value; }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Text;
+
+namespace LinqCustomTypeProject 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("VW Golf", 270952);
+            Car car2 = new Car("Opel Astra", 56079);
+            Car car3 = new Car("Audi A4", 52493);
+            Car car4 = new Car("Ford Focus", 51677);
+            Car car5 = new Car("Seat Leon", 42125);
+            Car car6 = new Car("VW Passat", 97586);
+            Car car7 = new Car("VW Polo", 69867);
+            Car car8 = new Car("Mercedes C-Class", 67549);
+
+
+            var cars = new List<Car> {
+                    car1, car2, car3, car4, car5, car6, car7, car8 };
+
+            var listCars = from car in cars 
+                           select car.Name; //get name(real value) of each object
+
+            foreach (var element in listCars)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("+++++++++++++++++");
+
+            var listUnitsSold = from car in cars 
+                                select car.UnitsSold; 
+
+            foreach (var element in listUnitsSold)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("+++++++++++++++++");
+
+            var listWithExpression = from car in cars
+                                     where car.UnitsSold > 60000
+                                     orderby car.UnitsSold descending
+                                     select car;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var car in listWithExpression)
+            {
+                stringBuilder.AppendLine($"{car.Name} - {car.UnitsSold}");
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+
+            Console.WriteLine("+++++++++++++++++");
+
+
+
+        }
+    }
+}
